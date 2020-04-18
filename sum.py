935e52013a1e62b44b5ba1fb1d94ae247d32a95c
parthaSdeb <parthadeb95@gmail.com>
2020-04-18 22:53:43 +0600

    smart summation code

--- sum.py
+++ sum.py
@@ -4,7 +4,8 @@ list =[1,2,3,4,5,6,7,8,9]
 def sum(test_list):
     summation = 0
     #do actual summ
-    
+    for i in range (len(test_list)): summation += test_list[i]
+    return summation
 
 
 print(sum(list))
